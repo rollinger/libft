f1709cb7389663f1c151f3c9bffc5afe478b463d
Philipp Rollinger <philipp.rollinger@gmail.com>
2021-05-20 20:49:31 +0200

    Add list last

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -1,28 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstsize.c                                       :+:      :+:    :+:   */
+/*   ft_lstlast.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: prolling <prolling@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/05/20 20:40:50 by prolling          #+#    #+#             */
-/*   Updated: 2021/05/20 20:45:21 by prolling         ###   ########.fr       */
+/*   Created: 2021/05/20 20:44:38 by prolling          #+#    #+#             */
+/*   Updated: 2021/05/20 20:46:43 by prolling         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
-* Counts the number of elements in a list. Returns lenght of list
+* Returns the last element of the list.
 */
 
-int ft_lstsize(t_list *lst)
+t_list *ft_lstlast(t_list *lst)
 {
-	size_t	len;
-
-	len = 1;
 	while (lst->next != NULL)
-	{
 		lst = lst->next;
-		++len;
-	}
-	return (len);
+	return (lst);
 }
