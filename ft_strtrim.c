a64b6958f33cbd1f329ac2c21110e86ea2edb4e8
Philipp Rollinger <philipp.rollinger@gmail.com>
2021-05-21 10:09:28 +0200

    Fix strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: prolling <prolling@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/20 09:44:26 by prolling          #+#    #+#             */
-/*   Updated: 2021/05/20 18:44:45 by prolling         ###   ########.fr       */
+/*   Updated: 2021/05/21 10:09:17 by prolling         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,56 +24,26 @@
 * 3. return the string
 * for strspn see man strspn
 */
-
-static size_t	ft_strspn(const char *s, const char *accept)
-{
-	size_t	len;
-
-	while (ft_strchr(accept, (int)*s) != 0)
-	{
-		++s;
-		++len;
-	}
-	return (len);
-}
-
-static char	*ft_lstrtrim(char *str, const char *set)
-{
-	size_t	totrim;
-	size_t	len;
-
-	totrim = ft_strspn(str, set);
-	if (totrim > 0)
-	{
-		len = ft_strlen(str);
-		if (totrim == len)
-			str[0] = '\0';
-		else
-			ft_memmove((void *)str, (void *)(str + totrim), len + 1 - totrim);
-	}
-	return (str);
-}
-
-static char	*ft_rstrtrim(char *str, const char *set)
-{
-	int	i;
-
-	i = ft_strlen(str) - 1;
-	while (i >= 0 && ft_strchr(set, str[i]) != 0)
-	{
-		str[i] = '\0';
-		--i;
-	}
-	return (str);
-}
-
-/*
-* return (ft_lstrtrim(ft_rstrtrim((char *)s1, set), set));
-ft_lstrtrim((char *)s1, set)
-ft_rstrtrim((char *)s1, set)
-*/
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	ft_rstrtrim((char *)s1, set);
-	return (ft_strdup(ft_lstrtrim((char *)s1, set)));
+	char	*result;
+	char	*s2;
+	size_t	start;
+	size_t	end;
+
+	s2 = (char *)s1;
+	result = 0;
+	if (s1 != 0 && set != 0)
+	{
+		start = 0;
+		end = ft_strlen(s1);
+		while (s1[start] && ft_strchr(set, s1[start]))
+			++start;
+		while (s1[end - 1] && ft_strchr(set, s1[end - 1]) && end > start)
+			--end;
+		result = (char *)malloc(sizeof(char) * (end - start + 1));
+		if (result)
+			ft_strlcpy(result, &s2[start], (end - start + 1));
+	}
+	return (result);
 }
