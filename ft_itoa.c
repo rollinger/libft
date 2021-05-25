33c50f716b3d8645b2ca4979dd5107132b245f05
Philipp Rollinger <philipp.rollinger@gmail.com>
2021-05-25 11:23:32 +0200

    rechecked malloc null cond.

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,13 +6,11 @@
 /*   By: prolling <prolling@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/20 11:17:28 by prolling          #+#    #+#             */
-/*   Updated: 2021/05/25 08:55:27 by prolling         ###   ########.fr       */
+/*   Updated: 2021/05/25 11:18:53 by prolling         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-void	*malloc(size_t size);
 
 /*
 * Allocates (with malloc(3)) and returns a string representing the integer
@@ -44,10 +42,10 @@ char	*ft_itoa(int n)
 	char	*r;
 	size_t	dc;
 
-	dc = ft_countd(n);
-	d = ft_calloc(sizeof(char), (dc + 1));
 	if (n == -2147483648)
 		return (ft_strdup("-2147483648"));
+	dc = ft_countd(n);
+	d = ft_calloc(sizeof(char), (dc + 1));
 	r = d;
 	if (n < 0)
 	{
